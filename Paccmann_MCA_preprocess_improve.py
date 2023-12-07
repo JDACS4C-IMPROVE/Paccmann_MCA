7ef4491d7d12f24aeb53866eb97bd827a0eb477c
priyanka9991 <pvasanthakumari@anl.gov>
2023-12-07 11:36:40 -0600

    modify comment

--- Paccmann_MCA_preprocess_improve.py
+++ Paccmann_MCA_preprocess_improve.py
@@ -200,7 +200,7 @@ def run(params):
     fname='Data_MCA.zip'
     origin=params['data_url']
     candle.file_utils.get_file(fname, origin)
-    #Other files needed for Paccmann_MCA
+    # Move model-specific files to ml_data_outdir
     shutil.copy(os.path.join(os.environ['CANDLE_DATA_DIR'],'common','Data','2128_genes.pkl'),os.path.join(Path(params["ml_data_outdir"]),'2128_genes.pkl') )
     shutil.copy(os.path.join(os.environ['CANDLE_DATA_DIR'],'common','Data','smiles_language_chembl_gdsc_ccle.pkl'),os.path.join(Path(params["ml_data_outdir"]),'smiles_language_chembl_gdsc_ccle.pkl') )
 
