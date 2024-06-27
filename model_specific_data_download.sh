e2abbd221bad5df952c7154ac8f723f274d9590f
priyanka9991 <pvasanthakumari@anl.gov>
2024-06-26 23:12:35 -0500

    Debug

--- model_specific_data_download.sh
+++ model_specific_data_download.sh
@@ -3,13 +3,16 @@
 DATA_URL=$1
 OUTPUT_DIR=$2
 
-if [ -f "$OUTPUT_DIR/Data" ]; then 
-  echo "Model specific data directory present, skipping download"
-elif [ ! -d $OUTPUT_DIR ]
-then
+if [ -d "$OUTPUT_DIR/Data" ]; then 
+    echo "Model specific data directory present, skipping download"
+elif [ ! -d $OUTPUT_DIR ]; then
     echo "Directory does not exist, Making an output directory"
     mkdir -p $OUTPUT_DIR
+    wget -P $OUTPUT_DIR $DATA_URL
+    unzip -d $OUTPUT_DIR "$OUTPUT_DIR/Data_MCA.zip" #TODO: CHANGE THIS 
+ else
+     wget -P $OUTPUT_DIR $DATA_URL
+     unzip -d $OUTPUT_DIR "$OUTPUT_DIR/Data_MCA.zip" #TODO: CHANGE THIS 
 fi
-wget -P $OUTPUT_DIR $DATA_URL
-unzip -d $OUTPUT_DIR "$OUTPUT_DIR/Data_MCA.zip" #TODO: CHANGE THIS 
+
 
