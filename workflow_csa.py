e2eb62db74256ec5e513698004fe88e8434ede2f
priyanka9991 <pvasanthakumari@anl.gov>
2024-07-24 15:57:53 -0500

    add config for polaris

--- workflow_csa.py
+++ workflow_csa.py
@@ -421,7 +421,7 @@ config_polaris = Config(
 train_futures=[]
 
 #parsl.load(local_config)
-parsl.load(local_config)
+parsl.load(config_lambda)
 for source_data_name in params['source_datasets']:
     for split in params['split']:
         for target_data_name in params['target_datasets']:
